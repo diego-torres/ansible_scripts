42c483ddd72800685fbbd288a7c7253fee09f281
jbride <jbride2001@yahoo.com>
2016-12-18 05:00:13 -0700

    deprecated jdg

--- other/jdg/docker/postgresql/bin/initdb.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-ls -l /
-id
-source /cluster/bin/setenv.sh
-initdb /pgdata
-
--- other/jdg/docker/postgresql/bin/standalone-setup.sql
+++
@@ -1,6 +0,0 @@
-ALTER USER postgres WITH PASSWORD 'postgres';
-create user jdg login superuser password 'jdg';
-
-# DROP DATABASE IF EXISTS jdgcachestore;
-CREATE DATABASE jdgcachestore;
-grant all privileges on database jdgcachestore to jdg;
