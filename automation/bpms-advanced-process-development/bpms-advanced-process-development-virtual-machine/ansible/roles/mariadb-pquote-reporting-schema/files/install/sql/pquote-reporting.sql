932e781e8aed23a5f113d8729a12f24012d86f2f
dtorresf <dtorresf@redhat.com>
2017-01-30 22:56:49 -0600

    https://github.com/redhat-gpe/bxms_development/issues/32  - mock policy quote reporting data

--- automation/bpms-advanced-process-development/bpms-advanced-process-development-virtual-machine/ansible/roles/mariadb-pquote-reporting-schema/files/install/sql/pquote-reporting.sql
+++ automation/bpms-advanced-process-development/bpms-advanced-process-development-virtual-machine/ansible/roles/mariadb-pquote-reporting-schema/files/install/sql/pquote-reporting.sql
@@ -49,3 +49,119 @@ CREATE TABLE `FACT_INCIDENTS`
     `driver_adult` bit(1) NOT NULL DEFAULT 0,
     `driver_elder` bit(1) NOT NULL DEFAULT 0
 );
+
+DELIMITER //
+CREATE FUNCTION `randomRangePicker`
+(
+  minRange INT,
+  maxRange INT
+) RETURNS int(11)
+BEGIN
+  DECLARE pick INT;
+  SET pick = minRange + FLOOR(RAND() * (maxRange - minRange + 1));
+  RETURN pick;
+END //
+CREATE FUNCTION `randomSSN`
+() RETURNS VARCHAR(9)
+BEGIN
+  DECLARE ssn VARCHAR(9);
+  DECLARE i TINYINT;
+  SET i = 0;
+  SET SSN = CONCAT('', randomRangePicker(1,9));
+  label_s: LOOP
+    SET i = i + 1;
+    IF i < 8 THEN
+      SET SSN = CONCAT(SSN, randomRangePicker(0,9));
+      ITERATE label_s;
+    END IF;
+    LEAVE label_s;
+  END LOOP label_s;
+  RETURN ssn;
+END //
+CREATE PROCEDURE prc_generate_quote_mock (IN idx INT)
+BEGIN
+  DECLARE lAgeMin INT;
+  DECLARE lAgeMax INT;
+  DECLARE lAge INT;
+
+  DECLARE lBounced BIT;
+  DECLARE lBounceReasonCode TINYINT;
+  SET lBounceReasonCode = randomRangePicker(0,9);
+  SET lBounced = 0;
+
+  SET lAgeMin = 16;
+  SET lAgeMax = 80;
+  SET lAge = randomRangePicker(lAgeMin, lAgeMax);
+
+  CASE WHEN randomRangePicker(1,4) < 4
+    THEN
+      SET lBounced = 0;
+    ELSE
+      BEGIN
+        SET lBounced = 1;
+      END;
+  END CASE;
+
+  INSERT INTO FACT_QUOTES(
+      age,
+      creditScore,
+      dlnumber,
+      driverName,
+      numberOfAccidents,
+      numberOfTickets,
+      ssn,
+      policyType,
+      price,
+      priceDiscount,
+      vehicleYear,
+      processInstanceId,
+      quote_month,
+      quote_year,
+      purchased,
+      bounced,
+      bounced_price,
+      bounced_beneffits,
+      bounced_terms,
+      bounced_service,
+      bounced_other)
+  VALUES(
+    lAge,
+    randomRangePicker(250, 750),
+    CONCAT('DL', randomRangePicker(1000, 10000)),
+    CONCAT_WS(' ', 'DRIVER', randomRangePicker(1000, 10000)),
+    randomRangePicker(0,4),
+    randomRangePicker(0,4),
+    randomSSN(),
+    'CAR',
+    randomRangePicker(150,750),
+    randomRangePicker(0,25),
+    randomRangePicker(2007, 2017),
+    idx,
+    randomRangePicker(1,12),
+    randomRangePicker(2015,2016),
+    CASE WHEN lBounced = 0 THEN 1 ELSE 0 END,
+    lBounced,
+    CASE WHEN lBounceReasonCode <= 1 AND lBounced = 1 THEN 1 ELSE 0 END,
+    CASE WHEN lBounceReasonCode <= 2 AND lBounceReasonCode > 1 AND lBounced = 1 THEN 1 ELSE 0 END,
+    CASE WHEN lBounceReasonCode <= 5 AND lBounceReasonCode > 1 AND lBounced = 1 THEN 1 ELSE 0 END,
+    CASE WHEN lBounceReasonCode > 5 AND lBounceReasonCode < 9 AND lBounced = 1 THEN 1 ELSE 0 END,
+    CASE WHEN lBounceReasonCode = 9 AND lBounced = 1 THEN 1 ELSE 0 END
+  );
+END //
+
+CREATE PROCEDURE prc_generate_mocks (IN records INT)
+BEGIN
+  DECLARE i INT;
+  SET i = 0;
+  label_q: LOOP
+    SET i = i + 1;
+    IF i < records THEN
+      CALL prc_generate_quote_mock(i);
+      ITERATE label_q;
+    END IF;
+    LEAVE label_q;
+  END LOOP label_q;
+  SET i = 0;
+END //
+DELIMITER ;
+CALL prc_generate_mocks(1500);
